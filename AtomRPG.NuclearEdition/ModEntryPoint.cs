a9444376760349397af89d73f369a8d53af89f93
Albeoris <albeoris@gmail.com>
2019-01-10 21:25:52 +0300

    Working after the exit to the main menu

--- AtomRPG.NuclearEdition/ModEntryPoint.cs
+++ AtomRPG.NuclearEdition/ModEntryPoint.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using UnityEngine;
 
 namespace AtomRPG.NuclearEdition
@@ -9,8 +10,18 @@ namespace AtomRPG.NuclearEdition
     // This component will be instantiated while loading mods. You control the lifetime of the object.
     public class ModEntryPoint : MonoBehaviour
     {
+        private Single _recheckTimeSec = 0;
+
         void Update()
         {
+            // Exit to the main menu unloads game scenes and interface objects. We must ensure that our mod remains active.
+            // Recheck it every 10 seconds
+            _recheckTimeSec += Time.deltaTime;
+            if (_recheckTimeSec < 10.0 /*sec*/)
+                return;
+
+            _recheckTimeSec = 0;
+
             // Don't use Awake()
             // Now the initialization of mods occurs before the initialization of the environment of the game.
             // We have to wait for it to be initialized.
@@ -21,15 +32,19 @@ namespace AtomRPG.NuclearEdition
             // Initialize our mod
             Initialize(barterHud);
 
-            // We no longer need this component. Destroy the object.
-            Destroy(this.gameObject);
+            // We can destroy this component after initialization but this mod will be unloaded when the player exit to the main menu. Leave it active in the background.
+            // Destroy(this.gameObject);
         }
 
         private void Initialize(BarterHUD barterHud)
         {
-            // Add our own component to an existing interface object.
-            ExtendedBarterHUD extendedHud = barterHud.gameObject.AddComponent<ExtendedBarterHUD>();
-            extendedHud.Initialize(barterHud);
+            ExtendedBarterHUD extendedHud = barterHud.gameObject.GetComponent<ExtendedBarterHUD>();
+            if (extendedHud == null)
+            {
+                // Add our own component to an existing interface object.
+                extendedHud = barterHud.gameObject.AddComponent<ExtendedBarterHUD>();
+                extendedHud.Initialize(barterHud);
+            }
         }
     }
 }
