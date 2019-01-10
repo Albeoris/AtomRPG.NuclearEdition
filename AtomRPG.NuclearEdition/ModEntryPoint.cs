d96bc79f1ba65ce4cb96348f86c9f3ff8083fabd
Albeoris <albeoris@gmail.com>
2019-01-10 10:08:57 +0300

    Comments

--- AtomRPG.NuclearEdition/ModEntryPoint.cs
+++ AtomRPG.NuclearEdition/ModEntryPoint.cs
@@ -3,20 +3,31 @@ using UnityEngine;
 
 namespace AtomRPG.NuclearEdition
 {
+    // This is the entry point.
+    // It should be named ModEntryPoint.
+    // It should be inherited from MonoBehaviour.
+    // This component will be instantiated while loading mods. You control the lifetime of the object.
     public class ModEntryPoint : MonoBehaviour
     {
         void Update()
         {
-            var barterHud = Game.World?.HUD?.Barter;
+            // Don't use Awake()
+            // Now the initialization of mods occurs before the initialization of the environment of the game.
+            // We have to wait for it to be initialized.
+            BarterHUD barterHud = Game.World?.HUD?.Barter;
             if (barterHud == null) // Not yet initialized
                 return;
 
+            // Initialize our mod
             Initialize(barterHud);
+
+            // We no longer need this component. Destroy the object.
             Destroy(this.gameObject);
         }
 
         private void Initialize(BarterHUD barterHud)
         {
+            // Add our own component to an existing interface object.
             ExtendedBarterHUD extendedHud = barterHud.gameObject.AddComponent<ExtendedBarterHUD>();
             extendedHud.Initialize(barterHud);
         }
--- README.md
+++ README.md
@@ -13,3 +13,39 @@ Modification for the game Atom RPG
 # Todo
 1. Resolve game path via Windows Registry
 2. Generate .mdb symbols after build
+
+# Feautures
+1. You can switch between characters during barter
+
+# Current loader
+
+```string modsFolder = Path.GetFullPath("Mods");
+if (Directory.Exists(modsFolder))
+{
+	Debug.Log("Looking for mods (" + modsFolder + ")...");
+	foreach (string fileName in Directory.GetFiles(modsFolder, "*.dll", SearchOption.AllDirectories))
+	{
+		Debug.Log("Found assembly (" + fileName + "). Looking for entry points...");
+		try
+		{
+			foreach (Type type in Assembly.LoadFrom(fileName).GetTypes())
+			{
+				if (type.Name == "ModEntryPoint")
+				{
+					Debug.Log("Found entry point (" + type.FullName + "). Initializing...");
+					GameObject gameObject = new GameObject(fileName + "_" + type.FullName);
+					gameObject.AddComponent(type);
+					Object.DontDestroyOnLoad(gameObject);
+				}
+			}
+		}
+		catch (Exception ex)
+		{
+			Debug.LogError(string.Format("Failed to load assembly {0}. Error: {1}", fileName, ex));
+		}
+	}
+}
+else
+{
+	Debug.Log("Mods directory is not exists (" + modsFolder + ")");
+}```
